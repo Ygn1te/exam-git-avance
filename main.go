0e72964a8692fdad3da4875ed46e84c8ea99166c
lucas-sv <144680462+waytix-l@users.noreply.github.com>
2025-11-26 16:22:32 +0100

    fix server

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"log"
-	"main/server"
+	"main/handlers"
 	"net/http"
 )
 
 func main() {
 	log.Println("Serveur Go en démarrage...")
 
-	http.HandleFunc("/", server.HomeHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
